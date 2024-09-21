9c21a06194e2ef8e96f7a9bbd37078e30a92f3f2
hajime.takeuchi <shadowthe8156@gmail.com>
2024-09-21 16:02:09 +0900

    add express and sample api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log("hello world!!!!");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const server = app.listen(3000, function() {
+  console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.disable("x-powerd-by");
+app.use(cors()).use(express.json());
+
+app.get("/",(req, res) => {
+  // res.send("hello world!!");
+  res.json({
+    id: "1",
+    name: "hogehoge",
+    description: "hello world!!!",
+  });
+});
